f627643f1061a6524df9c6c012aba832781d7b31
Ed Sumerfield <esumerfd@bitbashers.org>
2013-02-25 20:44:59 -0500

    Add interactive id and descriptor that refrences it

--- lib/mia.rb
+++ lib/mia.rb
@@ -2,4 +2,8 @@ class Mia
   def hello
     "hello"
   end
+
+  def interactive_id
+    "test_#{Random.rand(1000)}"
+  end
 end
--- spec/mia_spec.rb
+++ spec/mia_spec.rb
@@ -6,4 +6,8 @@ describe "mia" do
   it "returns text" do
     Mia.new.hello.should == "hello"
   end
+
+  it "returns a random interactive_id" do
+    Mia.new.interactive_id.should start_with("test_")
+  end
 end
